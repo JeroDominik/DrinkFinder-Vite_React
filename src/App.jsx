64a8968f9213cda7cf466648645fea7b2ef0d34e
Jero Dominik <jeronimo.dominik@gmail.com>
2023-08-01 18:52:38 -0300

    Comenzando App Buscador de Tragos

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { Container } from 'react-bootstrap'
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <header className='py-5'>
+        <h1>Buscador de Tragos</h1>
+      </header>
+
+      <Container className='mt-5'>
+        <form action="">
+          Aqui va el Form
+        </form>
+      </Container>
+    </>
+
+  )
+}
+
+export default App
